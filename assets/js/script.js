491b7ca42238ad770192078f30902097e820ceb7
cafalchio <maolivei@tcd.ie>
2022-09-24 18:22:34 +0100

    added pieces to board

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,32 +11,85 @@ class Board {
       const newDiv = document.createElement("div");
       newDiv.className = "square";
       newDiv.classList.add(color);
+      newDiv.id = i;
+      // newDiv.setAttribute("ondrop", "drop(event)");
+      // newDiv.setAttribute("ondragover", "allowDrop(event)");
+      newDiv.addEventListener("dragover", dragOver);
+      newDiv.addEventListener("dragenter", dragEnter);
+      newDiv.addEventListener("dragleave", dragLeave);
+      newDiv.addEventListener("drop", dragDrop);
       this.game.appendChild(newDiv);
-
-      // Create initial pieces
-      // if ((i != 3) & (i != 4)) {
-      //   if ((i % 2 == 0) & (j % 2 != 0) & (i < 3)) {
-      //     span.classList.add("wPiece");
-      //   } else if ((i % 2 != 0) & (j % 2 == 0) & (i < 3)) {
-      //     span.classList.add("wPiece");
-      //   } else if ((i % 2 == 0) & (j % 2 != 0) & (i > 4)) {
-      //     span.classList.add("bPiece");
-      //   } else if ((i % 2 != 0) & (j % 2 == 0) & (i > 4)) {
-      //     span.classList.add("bPiece");
-      //   }
-      // }
-    } // end for
-  } // end constructor
-
-  getPosition() {
-    // for this.game.ele
+    }
   }
 
-  updateMove() {}
-
-  isOver() {}
+  startGame() {
+    const blackPieces = [1, 3, 5, 7, 8, 10, 12, 14, 17, 19, 21, 23];
+    const whitePieces = [40, 42, 44, 46, 49, 51, 53, 55, 56, 58, 60, 62];
+    // create pieces
+    for (let i = 0; i < 64; i++) {
+      const newPiece = new Piece("black");
+      document.getElementById(blackPieces[i]).appendChild(newPiece.get_piece());
+      const newPiece2 = new Piece("white");
+      document
+        .getElementById(whitePieces[i])
+        .appendChild(newPiece2.get_piece());
+    }
+  }
 }
 
+// Piece class
+class Piece {
+  constructor(color) {
+    this.color = color;
+    this.isQueen = false;
+    this.isTaken = false;
+    this.piece = document.createElement("div");
+    this.piece.className = "piece";
+    this.piece.classList.add("piece-" + color);
+    this.piece.draggable = true;
+    // make it draggable
+    this.piece.addEventListener("dragstart", dragStart);
+    this.piece.addEventListener("dragend", dragEnd);
+  }
+  // create a new piece
+  get_piece() {
+    return this.piece;
+  }
+}
+
+// Drag and drop
+function allowDrop(ev) {
+  ev.preventDefault();
+}
 // create board
 const board = new Board();
-board.getPosition();
+board.startGame();
+
+// Drag Functions
+
+function dragStart() {
+  this.className += " hold";
+  setTimeout(() => (this.className = "invisible"), 0);
+}
+
+function dragEnd() {
+  this.className = "piece";
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += " hovered";
+}
+
+function dragLeave() {
+  this.className = "square";
+}
+
+function dragDrop() {
+  this.className = "square";
+  this.append(piece);
+}
