dd61aa1a0fad0610f6018a8df1feb272c0840175
cafalchio <maolivei@tcd.ie>
2022-09-25 23:47:19 +0100

    added method to check possible piece movement

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,28 +12,109 @@ class Board {
       newDiv.className = "square";
       newDiv.classList.add(color);
       newDiv.id = i;
-      // newDiv.setAttribute("ondrop", "drop(event)");
-      // newDiv.setAttribute("ondragover", "allowDrop(event)");
-      newDiv.addEventListener("dragover", dragOver);
-      newDiv.addEventListener("dragenter", dragEnter);
-      newDiv.addEventListener("dragleave", dragLeave);
-      newDiv.addEventListener("drop", dragDrop);
+      // newDiv.innerText = newDiv.id;
       this.game.appendChild(newDiv);
     }
   }
 
   startGame() {
+    // start place for pieces
     const blackPieces = [1, 3, 5, 7, 8, 10, 12, 14, 17, 19, 21, 23];
     const whitePieces = [40, 42, 44, 46, 49, 51, 53, 55, 56, 58, 60, 62];
+    const squares = document.querySelectorAll(".square");
+
+    // dragstarted event
+    document.addEventListener("dragstart", (e) => {
+      e.target.classList.add("dragging");
+      const possibleTarget = this.checkPossibleTarget(e.target.id);
+      e.dataTransfer.setData("targets", possibleTarget);
+      console.log(possibleTarget);
+    });
+    // dragended event
+    document.addEventListener("dragend", (e) => {
+      e.target.classList.remove("dragging");
+      const data = e.dataTransfer.getData("targets");
+      let possibleTarget = data.split(",");
+      let targets = possibleTarget.map((item) => parseInt(item));
+      if (targets.includes(e.target.id)) {
+        console.log("ok");
+      }
+    });
     // create pieces
     for (let i = 0; i < 64; i++) {
-      const newPiece = new Piece("black");
-      document.getElementById(blackPieces[i]).appendChild(newPiece.get_piece());
-      const newPiece2 = new Piece("white");
-      document
-        .getElementById(whitePieces[i])
-        .appendChild(newPiece2.get_piece());
+      const bPiece = new Piece("black");
+      const wPiece = new Piece("white");
+      if (blackPieces.includes(i)) {
+        squares[i].appendChild(bPiece.get_piece(100 + i));
+      }
+      if (whitePieces.includes(i)) {
+        squares[i].appendChild(wPiece.get_piece(200 + i));
+      }
+      // add event listeners
+      squares[i].addEventListener("dragover", (e) => {
+        const dragging = document.querySelector(".dragging");
+        // const applyAfter = this.movedPiece(squares[i]);
+        // console.log("dragover " + e.target.id);
+      });
     }
+  }
+  // Method to check possible targets
+  checkPossibleTarget(id) {
+    // left corners
+    const leftCorners = [8, 16, 24, 32, 40, 48, 56];
+    // right corners
+    const rightCorners = [7, 15, 23, 31, 39, 47, 55, 63];
+    // get piece
+    const movPiece = document.querySelector(".dragging");
+    // get piece color
+    const pieceColor = movPiece.classList[1];
+    // get piece id
+    const pieceId = parseInt(movPiece.parentNode.id);
+    // get squares
+    const squares = document.querySelectorAll(".square");
+    let possibleTargets = [];
+    // check if is a Queen
+    if (movPiece.isQueen) {
+      possibleTargets.push(pieceId - 9);
+      possibleTargets.push(pieceId - 7);
+      possibleTargets.push(pieceId + 7);
+      possibleTargets.push(pieceId + 9);
+      // console.log("queen");
+    } else {
+      switch (pieceColor) {
+        case "piece-white":
+          possibleTargets.push(pieceId - 9);
+          possibleTargets.push(pieceId - 7);
+          if (leftCorners.includes(pieceId)) {
+            possibleTargets.shift();
+          }
+          if (rightCorners.includes(pieceId)) {
+            possibleTargets.pop();
+          }
+          break;
+        case "piece-black":
+          possibleTargets.push(pieceId + 9);
+          possibleTargets.push(pieceId + 7);
+          if (leftCorners.includes(pieceId)) {
+            possibleTargets.pop();
+          }
+          if (rightCorners.includes(pieceId)) {
+            possibleTargets.shift();
+          }
+          break;
+      }
+    }
+    // check for pieces on the way
+    for (let i = 0; i < possibleTargets.length; i++) {
+      if (possibleTargets[i] < 0 || possibleTargets[i] > 63) {
+        possibleTargets.splice(i, 1);
+      } else {
+        if (squares[possibleTargets[i]].hasChildNodes()) {
+          possibleTargets.splice(i, 1);
+        }
+      }
+    }
+    return possibleTargets;
   }
 }
 
@@ -47,49 +128,14 @@ class Piece {
     this.piece.className = "piece";
     this.piece.classList.add("piece-" + color);
     this.piece.draggable = true;
-    // make it draggable
-    this.piece.addEventListener("dragstart", dragStart);
-    this.piece.addEventListener("dragend", dragEnd);
   }
   // create a new piece
-  get_piece() {
+  get_piece(id) {
+    this.piece.id = id;
     return this.piece;
   }
 }
 
-// Drag and drop
-function allowDrop(ev) {
-  ev.preventDefault();
-}
 // create board
 const board = new Board();
 board.startGame();
-
-// Drag Functions
-
-function dragStart() {
-  this.className += " hold";
-  setTimeout(() => (this.className = "invisible"), 0);
-}
-
-function dragEnd() {
-  this.className = "piece";
-}
-
-function dragOver(e) {
-  e.preventDefault();
-}
-
-function dragEnter(e) {
-  e.preventDefault();
-  this.className += " hovered";
-}
-
-function dragLeave() {
-  this.className = "square";
-}
-
-function dragDrop() {
-  this.className = "square";
-  this.append(piece);
-}
