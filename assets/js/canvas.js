0e66ca9b00b5020fbaa3d51b7458beda60404137
cafalchio <maolivei@tcd.ie>
2022-10-01 14:09:59 +0100

    added docstring to Methods

---
+++ assets/js/canvas.js
@@ -0,0 +1,11 @@
+// Create a menu in the canvas with the following options:
+// Play, Rules, About
+// When the user clicks on the menu, the corresponding screen should be displayed
+// The user should be able to navigate back to the menu from any screen
+
+// Start page options
+function createMenu() {
+  console.log("end of picture");
+}
+
+createMenu();
